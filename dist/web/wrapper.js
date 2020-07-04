216a3e4e3414c4d120b0bd295a3c5d938156ec4b
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-07-04 20:41:40 +0530

    showReactDomPatchNotification: false

--- dist/web/wrapper.js
+++ dist/web/wrapper.js
@@ -1,2 +1,5 @@
+require('react-hot-loader').setConfig({
+    showReactDomPatchNotification: false
+})
 FireJSX.app = require('react-hot-loader/root').hot(
     (props) => React.createElement(__FIREJSX_APP__.default, props))
