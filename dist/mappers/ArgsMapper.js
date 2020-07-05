c92ad8cb318ed9be36eae1df29b88d3b284b2025
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-07-05 13:12:28 +0530

    new Link update

--- dist/mappers/ArgsMapper.js
+++ dist/mappers/ArgsMapper.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getArgs = void 0;
 const SmartArg_1 = require("smartarg/SmartArg");
 function getArgs() {
     return new SmartArg_1.default()
