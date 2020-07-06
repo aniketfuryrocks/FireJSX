5343f4d59a55fef1330e71c77a3cd907a804220d
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-07-06 22:04:02 +0530

    FlySSR free from plugins

--- src/FlySSR.ts
+++ src/FlySSR.ts
@@ -1,51 +1,39 @@
-import {Config} from "./mappers/ConfigMapper";
+import "./GlobalsSetter"
 
-require("./GlobalsSetter")
-
-import GlobalPlugin from "./plugins/GlobalPlugin";
 import Page from "./classes/Page";
 import StaticArchitect from "./architects/StaticArchitect";
 import {join} from "path";
-import {mapPlugin} from "./mappers/PluginMapper";
 import {FIREJSX_MAP, PathRelatives} from "./FireJSX";
 import * as fs from "fs"
-import {Args} from "./mappers/ArgsMapper";
+import {JSDOM} from "jsdom";
 
 export default class {
     readonly pageMap: Map<string, Page> = new Map()
     readonly renderer: StaticArchitect;
     readonly rel: PathRelatives;
     readonly rootDir: string;
-    readonly globalPlugins: GlobalPlugin[] = [];
 
     constructor(pathToLibDir: string, rootDir: string = process.cwd()) {
-        const firejs_map: FIREJSX_MAP = JSON.parse(fs.readFileSync(join(this.rootDir = rootDir, pathToLibDir, "firejsx.map.json")).toString());
-        firejs_map.staticConfig.pathToLib = join(rootDir, pathToLibDir);
-        this.rel = firejs_map.staticConfig.rel
-        this.renderer = new StaticArchitect(firejs_map.staticConfig);
-        for (const __page in firejs_map.pageMap) {
+        const firejsx_map: FIREJSX_MAP = JSON.parse(fs.readFileSync(join(this.rootDir = rootDir, pathToLibDir, "firejsx.map.json")).toString());
+        firejsx_map.staticConfig.pathToLib = join(rootDir, pathToLibDir);
+        this.rel = firejsx_map.staticConfig.rel
+        this.renderer = new StaticArchitect(firejsx_map.staticConfig);
+        for (const __page in firejsx_map.pageMap) {
             const page = new Page(__page);
-            page.chunks = firejs_map.pageMap[__page];
+            page.chunks = firejsx_map.pageMap[__page];
             this.pageMap.set(__page, page);
         }
     }
 
-    async loadGlobalPlugin(pluginPath: string, config: Config = {}, args:Args = {_: []}) {
-        const gp: GlobalPlugin[] = [];
-        await mapPlugin(pluginPath, {pageMap: this.pageMap, rootPath: this.rootDir, globalPlugins: gp, config, args});
-        gp.forEach(plug => this.renderer.renderGlobalPlugin(plug))
-        this.globalPlugins.push(...gp);
-    }
-
     async render(page: string, path: string, content: any = {}): Promise<{
-        html: string,
+        dom: JSDOM,
         map: string
     }> {
         const _page = this.pageMap.get(page);
         if (!page)
             throw new Error(`Page ${page} does not exist`)
         return {
-            html: await this.renderer.render(_page, path, content),
+            dom: await this.renderer.render(_page, path, content),
             map: `FireJSX.map=${JSON.stringify({
                 content,
                 chunks: _page.chunks
