36570f46458eecef89d6dbf0d55873570262473d
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-07-13 13:49:31 +0530

    rm sass

--- src/architects/WebpackArchitect.ts
+++ src/architects/WebpackArchitect.ts
@@ -70,8 +70,7 @@ export default class {
                                         hashPrefix: 'hash',
                                     },
                                 },
-                            },
-                            'sass-loader',
+                            }
                         ]
                     }]
             },
