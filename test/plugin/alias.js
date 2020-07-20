cc79594ae8fcc3a7ccbd4e65fd8f1661b75769f1
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-07-20 17:29:07 +0530

    test alias.js

--- test/plugin/alias.js
+++ test/plugin/alias.js
@@ -2,7 +2,7 @@ const {join} = require("path");
 
 exports.default = ({initWebpack}, $) => {
     if ($.pageArchitect.webpackArchitect.proOrSSR)
-        initWebpack("*", config => {
+        initWebpack(config =>
             config.resolve.alias = {'firejsx/Hot': join(__dirname, "../../src/components/Hot.js")}
-        })
+        )
 }
