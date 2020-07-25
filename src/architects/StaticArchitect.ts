e5b017cf9bc4cb9c91a8352166c5c9116284acfa
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-07-25 16:09:44 +0530

    init

--- src/architects/StaticArchitect.ts
+++ src/architects/StaticArchitect.ts
@@ -1,13 +1,10 @@
-import {join, resolve} from "path"
+import {join} from "path"
 import Page from "../classes/Page";
-import {JSDOM} from "jsdom"
 import {Helmet} from "react-helmet"
-import FireJSX from "../FireJSX";
 
 export interface StaticConfig {
     lib: string,
     outDir: string,
-    template: string | any,
     externals: string[],
     ssr: boolean,
     prefix: string,
@@ -15,126 +12,103 @@ export interface StaticConfig {
     fullExternalPath: string,
 }
 
-export interface StaticData extends StaticConfig {
-    template: JSDOM
+import {window} from 'ssr-window';
+import {link} from "fs";
+// @ts-ignore   | Spread Copy all properties from window to global
+global = {
+    ...global,
+    ...window
 }
+// @ts-ignore   | Make window object circular
+global.window = global
 
 export default class {
-    config: StaticData
+    config: StaticConfig
 
     constructor(param: StaticConfig) {
         this.config = param;
-        //global.window circular
-        // @ts-ignore
-        global.window = global;
-        global.FireJSX = {
-            lib: param.lib,
-            isSSR: param.ssr,
-            staticPrefix: this.config.staticPrefix,
-            prefix: this.config.prefix
-        }
-        //init JSDOM
-        this.config.template = new JSDOM(param.template)
-        //init template
-        {
-            const script = this.config.template.window.document.createElement("script");
-            script.innerHTML = `window.FireJSX =` + JSON.stringify({
-                isHydrated: param.ssr,
-                lib: param.lib,
-                prefix: param.prefix,
-                staticPrefix: param.staticPrefix
-            })
-            this.config.template.window.document.head.appendChild(script);
-        }
-        {
-            const meta = this.config.template.window.document.createElement("meta");
-            meta.content = `FireJSX v${global.__FIREJSX_VERSION__}`;
-            meta.name = "generator";
-            this.config.template.window.document.head.appendChild(meta);
-        }
-        //require uncached to prevent bugs in lambda because node clears these 2 when a new request is assigned
-        //if ssr then load react,react dom,LinkApi,ReactDOMServer chunks
         if (param.ssr)
             require(param.fullExternalPath)
-        else //just load LinkApi
-            require(resolve(__dirname, "../web/LinkApi"))
     }
 
-    render(page: Page, path: string, content: any): JSDOM {
-        //template serialize to prevent overwriting
-        const dom = new JSDOM(this.config.template.serialize(), {
-            url: "http://localhost:5000" + this.config.prefix + path,
-        });
-        //load stuff from dom.window to global
-        for (const domKey of ["document", "location", "history", "navigator", "screen", "matchMedia", "getComputedStyle"])
-            global[domKey] = dom.window[domKey];
+    render(page: Page, path: string, content: any): string {
         //globals
+        global.FireJSX.lib = this.config.lib
+        global.FireJSX.isSSR = this.config.ssr
+        global.FireJSX.staticPrefix = this.config.staticPrefix
+        global.FireJSX.prefix = this.config.prefix
         global.FireJSX.map = {
             content,
             chunks: page.chunks
         };
-        //isSSR
-        global.FireJSX.isSSR = this.config.ssr
-        //chunks
+        //if ssr then require async chunks
+        if (this.config.ssr)
+            page.chunks.async.forEach(chunk => {
+                if (chunk.endsWith(".js"))
+                    require(`${this.config.outDir}/${this.config.lib}/${chunk}`)
+            })
+        let head, body;
+        //map
         {
-            //preload and load page map
-            {
-                const link = document.createElement("link");
-                const script = document.createElement("script");
-                script.src = link.href = global.FireJSX.linkApi.getMapUrl(path);
-                link.rel = "preload";
-                script.crossOrigin = link.crossOrigin = "anonymous";
-                link.setAttribute("as", "script");
-                document.head.appendChild(link);
-                document.body.appendChild(script);
-            }
-            //external group semi
-            this.loadChunks([this.config.externals[this.config.ssr ? 1 : 0]], false)
-            //async
-            if (this.config.ssr)
-                page.chunks.async.forEach(chunk => {
-                    if (chunk.endsWith(".js"))
-                        require(`${this.config.outDir}/${this.config.lib}/${chunk}`)
-                })
-            //initial
-            this.loadChunks(page.chunks.initial)
-            //entry
-            this.loadChunks(page.chunks.entry)
-            //App
-            if (!page.app)
-                page.app = global.FireJSX.app
+            const mapPath = `${this.config.prefix}/${this.config.lib}/map${path === "/" ? "/index" : path}.map.js`
+            head = `<link href="${mapPath}" as="script" rel="preload" crossorigin="anonymous"/>`
+            body = `<script src="${mapPath}" crossorigin="anonymous"></script>`
         }
-        //static render
-        if (this.config.ssr) {
-            document.getElementById("root").innerHTML = global.window.ReactDOMServer.renderToString(
-                global.React.createElement(page.app, {content: global.FireJSX.map.content})
-            )
+        //external mini
+        {
+            const arr = this.loadChunks(head, body, [this.config.externals[this.config.ssr ? 1 : 0]], false)
+            head = arr[0]
+            body = arr[1]
         }
-        //head
+        //initial chunks
+        {
+            const arr = this.loadChunks(head, body, page.chunks.initial)
+            head = arr[0]
+            body = arr[1]
+        }
+        //entry chunks
+        {
+            const arr = this.loadChunks(head, body, page.chunks.entry)
+            head = arr[0]
+            body = arr[1]
+        }
+        if (!page.app)
+            page.app = global.FireJSX.app
+        const rootDiv = this.config.ssr ? global.window.ReactDOMServer.renderToString(
+            global.React.createElement(page.app, {content: global.FireJSX.map.content})
+        ) : ""
         if (this.config.ssr) {
             const helmet = Helmet.renderStatic();
-            let head = ""
             for (let helmetKey in helmet)
                 head += helmet[helmetKey].toString()
-            document.head.innerHTML += head
         }
-        return dom
+
+        return "<!doctype html>" +
+            "<html lang=\"en\"><head>" +
+            "<meta charset=\"UTF-8\">" +
+            "<meta name=\"viewport\" content=\"width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1\">" +
+            "<meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">" +
+            head +
+            "</head><body><div id=\"root\">" +
+            rootDiv +
+            "</div>" +
+            body +
+            "</body></html>"
     }
 
-    private loadChunks(chunks: string[], _require = true) {
+    private loadChunks(head: string, body: string, chunks: string[], _require = true): [string, string] {
         chunks.forEach(chunk => {
-            if (chunk.endsWith(".css")) {
-                const link = document.createElement("link");
-                link.href = `${this.config.prefix}/${this.config.lib}/${chunk}`
-                link.rel = "stylesheet";
-                link.crossOrigin = "anonymous";
-                document.head.insertBefore(link, document.head.firstChild);
-            } else {
-                if (this.config.ssr && _require && chunk.endsWith(".js"))//only require javascript
+            const src = `${this.config.prefix}/${this.config.lib}/${chunk}`
+            if (chunk.endsWith(".js")) {
+                if (this.config.ssr && _require)
                     require(join(this.config.outDir, this.config.lib, chunk))
-                global.FireJSX.linkApi.preloadChunks([chunk]);
-                global.FireJSX.linkApi.loadChunks([chunk]);
-            }
+                head += `<link rel="preload" href="${src}" as="script" crossorigin="anonymous"/>`
+                body += `<script src="${src}" crossorigin="anonymous"></script>`
+            } else if (chunk.endsWith(".css"))
+                head = `<link rel="stylesheet" href="${src}" crossorigin="anonymous"/>` + head
+            else
+                body += `<link href="${src}" crossorigin="anonymous"/>`
         })
+        return [head, body]
     }
 }
