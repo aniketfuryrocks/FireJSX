518a0d2b26f58501e3dca3009178dd7b076a1f5f
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-10-18 22:33:50 +0530

    test loader

--- test/src/_pages/index.jsx
+++ test/src/_pages/index.jsx
@@ -1,4 +1,6 @@
 import LazyLoad from "../../../src/components/LazyLoad";
+import LoadingCircle from "../components/LoadingCircle/LoadingCircle";
+import Loader from "../../../src/components/Loader";
 
 export default function () {
     const Markdown = LazyLoad(() => import("../components/Markdown/Markdown.jsx"), () => require.resolveWeak("../components/Markdown/Markdown.jsx"))
@@ -8,6 +10,9 @@ export default function () {
             <Markdown>
                 # This is a lazy loaded component
             </Markdown>
+            <Loader effect={React.useEffect} delay={800}>
+                <LoadingCircle/>
+            </Loader>
         </div>
     )
 }
