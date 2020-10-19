142f33f8545c750e05a01917ef088b0cb2083b43
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-10-19 15:45:22 +0530

    comment

--- src/loader/wrapper.ts
+++ src/loader/wrapper.ts
@@ -4,12 +4,11 @@ export default function (source, map) {
     //if current file is not in the pages directory then do nothing
     if (!this.resourcePath.startsWith(this.query.pages_path))
         return void this.callback(null, source, map)
-    //import Wrap.js and pass app
+    //Wrap default export function in Wrap,pass hot in development
     source = source.replace("export default ", `require("${join(__dirname, "../web/Wrap.js")}").default(`)
     const functionEnd = source.lastIndexOf("}");
     source = source.substring(0, functionEnd + 1) + (
             this.query.proOrSSR ? `)` : `,require("react-hot-loader/root").hot)`) +
         source.substring(functionEnd + 1);
-    console.log(source)
     this.callback(null, source, map)
 }
