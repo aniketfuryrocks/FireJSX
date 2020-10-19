41c1f28ddd133aa7ff9e1a608bcb47688dc21f87
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-10-19 10:33:24 +0530

    comment

--- src/loader/wrapper.ts
+++ src/loader/wrapper.ts
@@ -1,12 +1,10 @@
 import {join} from "path"
 
 export default function (source, map) {
-    //if current file is not in the pages directory
-    //then do nothing
+    //if current file is not in the pages directory then do nothing
     if (!this.resourcePath.startsWith(this.query.pages_path))
         return void this.callback(null, source, map)
     //import Wrap.js and pass app
     source += `require("${join(__dirname, "../web/Wrap.js")}").default(module.exports);`;
-    console.log(source)
     this.callback(null, source, map)
 }
