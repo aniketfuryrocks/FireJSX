6d17076613b9b209d6ae1462f73d0258303c1bc0
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-10-26 23:21:00 +0000

    helper hook

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,17 +1,7 @@
 import {hot} from "react-hot-loader/root";
 import * as React from "react";
+import useAppHelper from "./helper";
 
-FireJSX.app = hot(({app, content}) => {
-    const [App, setApp] = React.useState(React.createElement(app, {content}));
-
-    React.useEffect(() => {
-        FireJSX.setApp = (app, content) => {
-            // @ts-ignore
-            setApp(React.createElement(app, {content}));
-        }
-        //make undefined to prevent illegal state access
-        return () => FireJSX.setApp = undefined
-    }, [])
-
-    return App
+FireJSX.app = hot(props => {
+    return useAppHelper(props)
 })
--- src/web/helper.ts
+++ src/web/helper.ts
@@ -1,8 +1,15 @@
-export default function (effect, setApp, setContent) {
-    /*effect(() => {
-        FireJSX.runApp = (App, Content) => {
-            setApp(App);
-            setContent(Content);
+import * as React from "react";
+
+export default function useAppHelper({app, content}) {
+    const [App, setApp] = React.useState(React.createElement(app, {content}));
+    React.useEffect(() => {
+        FireJSX.setApp = (app, content) => {
+            // @ts-ignore
+            setApp(React.createElement(app, {content}));
         }
-    }, [])*/
+        //make undefined to prevent illegal state access
+        return () => FireJSX.setApp = undefined
+    }, [])
+    return App;
 }
+
