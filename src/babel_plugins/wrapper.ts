511a1b91a899267a15949aefcdb9709aa7feecd6
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-10-30 16:24:40 +0530

    rm comment

--- src/babel_plugins/wrapper.ts
+++ src/babel_plugins/wrapper.ts
@@ -37,20 +37,3 @@ export default function ({types: t}) {
         }
     }
 }
-/*
-* import {join} from "path"
-
-export default function (source, map) {
-    //if current file is not in the pages directory then do nothing
-    if (!this.resourcePath.startsWith(this.query.pages_path))
-        return void this.callback(null, source, map)
-    //Wrap default export function in Wrap,pass hot in development
-    source = source.replace("export default ", `require("${join(__dirname, "../web/Wrap")}").default(`)
-    const functionEnd = source.lastIndexOf("}");
-    source = source.substring(0, functionEnd + 1) + (
-            this.query.proOrSSR ? `)` : `,require("react-hot-loader/root").hot)`) +
-        source.substring(functionEnd + 1);
-    //callback with new src
-    this.callback(null, source, map)
-}
-*/
